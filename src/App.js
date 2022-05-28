2cfed1b36642a8cda30aa3e37779794a59ae6e63
snipergn <46683784+snipergn@users.noreply.github.com>
2022-05-28 17:27:52 +0300

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: []
+    }
+  };
+  componentDidMount() {
+    fetch('https://swapi.dev/api/people/1/')
+    .then(response=> response.json())
+      .then(people => {this.setState({ 'people': people})});
+  }
+
+  render() {
+     let {name, mass, hair_color, skin_color} = this.state.people
+    return (
+      <div className ='container'>
+        <h1> Starwars API </h1>;
+        <p> name: <span className='power'>{name}</span> </p>;
+        <p> mass: <span>{mass}</span> </p>;
+        <p> hair color: <span>{hair_color}</span> </p>;
+        <p> skin_color: <span>{skin_color}</span> </p>;
+      </div>
+      
+    );
+}
 }
 
 export default App;
